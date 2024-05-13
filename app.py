93af3ad4d49d7ab8fa07c46c2b8b527c751e8805
Antoine Falisse <antoinefalisse@gmail.com>
2024-05-13 11:27:09 -0700

    attempt on prem workers

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import traceback
 import logging
 import glob
 import numpy as np
-from utilsAPI import getAPIURL, getWorkerType, getASInstance, unprotect_current_instance
+from utilsAPI import getAPIURL, getWorkerType, getASInstance, unprotect_current_instance, get_number_of_pending_trials
 from utilsAuth import getToken
 from utils import (getDataDirectory, checkTime, checkResourceUsage,
                   sendStatusEmail, checkForTrialsWithStatus)
@@ -30,7 +30,9 @@ t = time.localtime()
 # for removing AWS machine scale-in protection
 t_lastTrial = time.localtime()
 justProcessed = True
+with_on_prem = True
 minutesBeforeRemoveScaleInProtection = 5
+max_on_prem_pending_trials = 5
 
 while True:
     # Run test trial at a given frequency to check status of machine. Stop machine if fails.
@@ -38,6 +40,18 @@ while True:
         runTestSession(isDocker=isDocker)           
         t = time.localtime()
         initialStatusCheck = True
+
+    # When using autoscaling, if there are on-prem workers, then we will remove
+    # the instance scale-in protection if the number of pending trials is below
+    # a threshold so that the on-prem workers are prioritized.
+    if with_on_prem:
+        # Query the number of pending trials
+        pending_trials = get_number_of_pending_trials()
+        if autoScalingInstance and pending_trials < max_on_prem_pending_trials:
+            # Remove scale-in protection and sleep in the cycle so that the
+            # asg will remove that instance from the group.
+            unprotect_current_instance()
+            time.sleep(3600)
            
     # workerType = 'calibration' -> just processes calibration and neutral
     # workerType = 'all' -> processes all types of trials
@@ -54,16 +68,19 @@ while True:
     if r.status_code == 404:
         logging.info("...pulling " + workerType + " trials.")
         time.sleep(1)
-        # when using autoscaling, we will remove the instance scale-in protection if it hasn't
+        
+        # When using autoscaling, we will remove the instance scale-in protection if it hasn't
         # pulled a trial recently and there are no actively recording trials
         if (autoScalingInstance and not justProcessed and 
             checkTime(t_lastTrial, minutesElapsed=minutesBeforeRemoveScaleInProtection)):
             if checkForTrialsWithStatus('recording', hours=2/60) == 0:
-                # AWS CLI to remove machine scale-in protection
+                # Remove scale-in protection and sleep in the cycle so that the
+                # asg will remove that instance from the group.
                 unprotect_current_instance()
                 time.sleep(3600)
             else:
                 t_lastTrial = time.localtime()
+                
         if autoScalingInstance and justProcessed:
             justProcessed = False
             t_lastTrial = time.localtime()
