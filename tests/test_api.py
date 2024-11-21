6ff69d4c6897c33702ba29570bcee1c9e7ce3709
carmichaelong <carmichael.ong@gmail.com>
2024-11-20 17:09:45 -0800

    add pytest and fill out tests for requests retry

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,27 @@
+import logging
+import pytest
+import requests
+
 from utils import makeRequestWithRetry
 
-def test_makeRequestWithRetry(self):
-    test_URL = 'http://httpbin.org/status/404'
+class TestMakeRequestWithRetry:
+    logging.getLogger('urllib3').setLevel(logging.DEBUG)
 
-    makeRequestWithRetry('GET', test_URL)
+    def test_get(self):
+        # should work and return 200
+        response = makeRequestWithRetry('GET', 'https://httpbin.org/get')
+        assert response.status_code == 200
+        assert response.json()['url'] == 'https://httpbin.org/get'
+    
+    def test_500(self):
+        # use an error code that triggers retries (should show up in debug log)
+        with pytest.raises(Exception):
+            response_500 = makeRequestWithRetry('GET', 'https://httpbin.org/status/500', retries=3, backoff_factor=0.1)
+    
+    def test_404(self):
+        # error code that won't trigger retries
+        with pytest.raises(Exception):
+            response_404 = makeRequestWithRetry('GET', 'https://httpbin.org/status/404', retires=2, backoff_factor=0.01)
 
 if __name__ == '__main__':
     unittest.main()
