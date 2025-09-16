d0e176d8a211a548bbc8e138c90ef3f927d0d61f
parkersruth <psr23@uw.edu>
2025-09-16 08:55:35 -0700

    um

--- download_session.py
+++ download_session.py
@@ -6,20 +6,24 @@ import utils
 def download_session(sid, sdir):
     if Path(sdir).exists():
         return
-    utils.downloadSomeStuff(sid,
-                        justDownload=True,
-                        isDocker=False,
-                        data_dir=Path(sdir).parent,
-                        includeVideos=False)
+    # utils.downloadSomeStuff(sid,
+    #                     justDownload=True,
+    #                     isDocker=False,
+    #                     data_dir=Path(sdir).parent,
+    #                     includeVideos=False)
+    utils.download_session(
+            sid,
+            sessionBasePath=Path(sdir).parent,
+            zipFolder=False,
+            writeToDB=False,
+            downloadVideos=False,
+    )
 
 
-if __name__ == '__main__':
-    # datadir = Path(snakemake.output[0])
-    download_session(snakemake.wildcards['sid'], snakemake.output[0])
+# if __name__ == '__main__':
+#     download_session(snakemake.wildcards['sid'], snakemake.output[0])
 
 
-
-# sid = '9ce91ceb-2bf8-4544-ae22-3da5ad3cb9a1'
-
-# sdir = Path('datadir') / 'opencap_data' / sid
-# download_session(sid, sdir)
+sid = '5e6be6c9-f5d0-4005-90e2-86064e1ec629'
+sdir = Path('datadir') / 'opencap_data' / sid
+download_session(sid, sdir)
